7b223a82eb33cb2de049a88a9667a56ccebc9439
celiaf <feng598164096@qq.com>
2023-04-20 20:26:46 +0800

    create user image dir when register

--- src/main/java/com/example/demo/service/Impl/AccountServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/AccountServiceImpl.java
@@ -1,20 +1,29 @@
 package com.example.demo.service.Impl;
 
+import com.example.demo.common.Common;
+import com.example.demo.config.StorageConfig;
 import com.example.demo.domain.User;
 import com.example.demo.domain.vo.UserLoginVO;
 import com.example.demo.mapper.AccountMapper;
 import com.example.demo.service.AccountService;
 //import jakarta.annotation.Resource;
 //import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
     @Resource
     private AccountMapper accountMapper;
+
+    @Autowired
+    private StorageConfig storageConfig;
 
     @Override
     public User login(String email, String password) {
@@ -32,6 +41,19 @@ public class AccountServiceImpl implements AccountService {
         user.setEmail(vo.getEmail());
         user.setPassword(vo.getPassword());
         accountMapper.insertAccount(user);
+
+        System.out.println(user);
+
+        File clothesDirectory = new File(Paths.get(storageConfig.getBasePath())
+                .resolve(Common.USER + user.getUserId())
+                .resolve(Common.CLOTHES_DIR).toUri());
+        if (!clothesDirectory.exists()) clothesDirectory.mkdirs();
+
+        File suitDirectory = new File(Paths.get(storageConfig.getBasePath())
+                .resolve(Common.USER + user.getUserId())
+                .resolve(Common.SUIT_DIR).toUri());
+        if (!suitDirectory.exists()) suitDirectory.mkdirs();
+
         return user;
     }
 }
